da8d90af98876f3a167ed6a854ed49f86724b4ee
killshot13 <dmreh@outlook.com>
2021-03-01 12:09:26 -0600

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,48 @@
-console.log('Hello world!');
+const handleResponse = (
+  resolve: (data: string) => void,
+  reject: (data: Error) => void,
+  response: IncomingMessage
+): void => {
+  let chunk = '';
+  const { statusCode } = response;
 
-for (let i = 0; i < 12; i++) {
-  console.log(i);
-}
+  if (statusCode !== 200) {
+    reject(new Error('Server error'));
+  }
 
-let x = 7;
+  response
+    .setEncoding('utf8')
+    .on('error', reject)
+    .on('uncaughtException', reject)
+    .on('data', (data: string) => (chunk += data))
+    .on('end', () => {
+      resolve(chunk);
+    });
+};
+
+const ping = async (): Promise<string> =>
+  new Promise(
+    (resolve: (data: string) => void, reject: (data: Error) => void) => {
+      const post = request({
+        path: '/',
+        port: 8080,
+        method: 'GET',
+        hostname: 'localhost',
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      });
+      const curriedHandleResponse = (response: IncomingMessage) => {
+        handleResponse(resolve, reject, response);
+      };
+
+      post.write('ping');
+      post.on('response', curriedHandleResponse);
+      post.on('error', () => {
+        reject(new Error('Request error'));
+      });
+      post.end();
+    }
+  );
+
+ping().then(console.log).catch(console.error);
